9a61a78371ecb9d0471b01d57ebffe7c80f60a3a
Bord_bf73@student.itstep.org <Bord_bf73@student.itstep.org>
2024-03-03 18:44:30 +0200

    Add project files.

---
+++ Homework/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(208, 566);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(421, 559);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Побудувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(538, 538);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 569);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Оберіть функцію";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 614);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Homework/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Homework
+{
+    public partial class Form1 : Form
+    {
+        private int width;
+        private int height;
+        public Form1()
+        {
+            InitializeComponent();
+            width = pictureBox1.Width; 
+            height = pictureBox1.Height;
+            Activated += new EventHandler(Form1_Load);
+            button1.Click += new EventHandler(button1_Click);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen graphPen = new Pen(Color.Red, 3);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            string chosenFunction = comboBox1.SelectedItem.ToString();
+            switch (chosenFunction)
+            {
+                case "Лінійна":
+                    BuildLinearFunction(g, graphPen); break;
+                case "Пряма пропорціональність":
+                    BuildProportionalFunction(g, graphPen); break;
+                case "Квадратична":
+                    BuildQuadraticFunction(g, graphPen); break;
+            }
+            graphPen.Dispose();
+            g.Dispose();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            int x_axis = pictureBox1.Height / 2;
+            int y_axis = pictureBox1.Width / 2;
+            Pen axis_line = new Pen(Color.Black, 2);
+            g.DrawLine(axis_line, 0, x_axis, pictureBox1.Width, x_axis);
+            g.DrawLine(axis_line, y_axis, 0, y_axis, pictureBox1.Height);
+            for (int x = 0; x < pictureBox1.Width; x += 20)
+            {
+                g.DrawLine(Pens.SlateGray, x, 0, x, pictureBox1.Height);
+            }
+            for (int y = 0; y < pictureBox1.Height; y += 20)
+            {
+                g.DrawLine(Pens.SlateGray, 0, y, pictureBox1.Width, y);
+            }
+        }
+        private void BuildLinearFunction(Graphics g, Pen graphPen)
+        {
+            for (int x = 100; x < width - 50; x++)
+            {
+                int y = height - 50 - (x - 100); //Приклад лінійної функції
+                g.DrawLine(graphPen, x, y, x + 1, y + 1);
+            }
+        }
+        private void BuildProportionalFunction(Graphics g, Pen graphPen)
+        {
+            int k = 3;
+            for (int x = 100; x < width - 50; x++)
+            {
+                int y = height - 50 - (k * (x - 100)); //Приклад прямої пропорціональності 
+                g.DrawLine(graphPen, x, y, x + 1, y + 1);
+            }
+        }
+        private void BuildQuadraticFunction(Graphics g, Pen graphPen)
+        {
+            PointF p1 = new PointF(0, height - 50);
+            PointF p2 = new PointF(width - 50, height - 50);
+            PointF[] curvePoints = GenerateQuadraticPoints(p1, p2);
+            g.DrawCurve(graphPen, curvePoints);
+        }
+        private PointF[] GenerateQuadraticPoints(PointF p1, PointF p2)
+        {
+            int numPoints = 50; // Кількість точок параболи
+            PointF[] curvePoints = new PointF[numPoints];
+            float a = 2f;
+            float b = 0f;
+            float c = 0f;
+            //double discriminant = b * b - 4 * a * c;
+            for (int i = 0; i < numPoints; i++)
+            {
+                float t = i / (float)(numPoints - 1);
+                float x = (1 - t) * p1.X + t * p2.X;
+                double y = a * Math.Pow(x, 2) + b * x + c; // Приклад квадратичної функції
+                curvePoints[i] = new PointF(x, (float)y);
+            }
+            return curvePoints;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Лінійна");
+            comboBox1.Items.Add("Пряма пропоціональність");
+            comboBox1.Items.Add("Квадратична");
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
